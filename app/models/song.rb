10b3c64edcf94093cc8eb58bfc3d9a4bfbe77bfd
Flatiron School <github email address>
2016-09-23 10:47:10 -0400

    done

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,26 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    # binding.pry
+    if !self.artist.nil?
+      self.artist.name
+    end
   end
 
   def artist_name=(name)
+    # self.artist.try(name) = name
+    # binding.pry
+    self.artist = Artist.find_or_create_by(:name => name)
+
+    self.artist.name
   end
+
+
+#     # if !self.artist.nil?
+#     #   self.artist = name
+#     #   self.save
+#     # end
+#     # binding.pry
+# #
+#   end
 end
