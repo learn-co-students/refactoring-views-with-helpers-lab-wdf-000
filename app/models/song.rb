185d85a15ed679b801c4ba93ce4625dc17053838
Kelene Chung <simoy93@gmail.com>
2016-10-17 14:52:34 -0400

    DONE

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+  	self.artist.name
   end
 
   def artist_name=(name)
+  	art_var = Artist.find_or_create_by(name: name)
+  	self.artist = art_var
   end
 end
